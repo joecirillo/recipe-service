91c1a024c8bc075d7b7a3ff858a0d3d7ee1eecd4
Joe <48731689+joecirillo@users.noreply.github.com>
2024-09-22 20:06:54 -0400

    first commit

---
+++ src/test/java/com/foodiesfinds/recipe_service/RecipeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodiesfinds.recipe_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
