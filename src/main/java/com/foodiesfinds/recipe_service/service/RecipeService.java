e499747fb946ac54b7106807a8b02c7eff699ee9
Joe <48731689+joecirillo@users.noreply.github.com>
2024-09-26 21:05:37 -0400

    Setting up recipe repo and service

---
+++ src/main/java/com/foodiesfinds/recipe_service/service/RecipeService.java
@@ -0,0 +1,5 @@
+package com.foodiesfinds.recipe_service.service;
+
+public interface RecipeService {
+
+}
---
+++ src/main/java/com/foodiesfinds/recipe_service/service/implementation/RecipeServiceImpl.java
@@ -0,0 +1,6 @@
+package com.foodiesfinds.recipe_service.service.implementation;
+import com.foodiesfinds.recipe_service.service.RecipeService;
+
+public class RecipeServiceImpl implements RecipeService {
+
+}
