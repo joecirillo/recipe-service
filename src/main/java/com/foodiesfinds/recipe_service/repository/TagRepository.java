120eaf6513982cdba841cacc892e26f60a3b6d4f
Joe <48731689+joecirillo@users.noreply.github.com>
2026-01-27 00:15:18 -0600

    Create shell for some of the repositories

--- src/main/java/com/foodiesfinds/recipe_service/repository/TagRepository.java
+++ src/main/java/com/foodiesfinds/recipe_service/repository/TagRepository.java
@@ -1,4 +1,9 @@
 package com.foodiesfinds.recipe_service.repository;
 
-public interface TagRepository {
+import com.foodiesfinds.recipe_service.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Tag, Long> {
+
+  boolean existsByTagName(String tagName);
 }
