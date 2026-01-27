96433543a6251fb14e714b18ed291690c5ef17ba
Joe <48731689+joecirillo@users.noreply.github.com>
2026-01-27 00:20:58 -0600

    Adding ingredient service impl to be used later

--- src/main/java/com/foodiesfinds/recipe_service/service/implementation/IngredientServiceImpl.java
+++ src/main/java/com/foodiesfinds/recipe_service/service/implementation/IngredientServiceImpl.java
@@ -1,13 +1,19 @@
 package com.foodiesfinds.recipe_service.service.implementation;
 
 import com.foodiesfinds.recipe_service.dto.IngredientDTO;
+import com.foodiesfinds.recipe_service.entity.Ingredient;
 import com.foodiesfinds.recipe_service.mapper.IngredientMapper;
 import com.foodiesfinds.recipe_service.repository.IngredientRepository;
 import com.foodiesfinds.recipe_service.service.IngredientService;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 
 @RequiredArgsConstructor
+@Service
+@Transactional
 public class IngredientServiceImpl implements IngredientService {
 
   private final IngredientMapper ingredientMapper;
