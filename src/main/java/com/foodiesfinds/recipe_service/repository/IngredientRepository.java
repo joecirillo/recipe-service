fa0353436d07ac81764fa65eaec3ead0b0f4a724
Joe <48731689+joecirillo@users.noreply.github.com>
2026-04-16 00:10:56 -0700

    Adding new repositories

--- src/main/java/com/foodiesfinds/recipe_service/repository/IngredientRepository.java
+++ src/main/java/com/foodiesfinds/recipe_service/repository/IngredientRepository.java
@@ -3,6 +3,7 @@ package com.foodiesfinds.recipe_service.repository;
 import com.foodiesfinds.recipe_service.entity.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
@@ -10,4 +11,6 @@ public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
     boolean existsByName(String name);
 
     Optional<Ingredient> findByNameIgnoreCase(String name);
+
+    List<Ingredient> findByNameContainingIgnoreCase(String name);
 }
