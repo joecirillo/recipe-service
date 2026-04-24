69d81ec8c88e8f3a69afe57afe9ef1ce485b5560
Joe <48731689+joecirillo@users.noreply.github.com>
2026-04-23 23:40:30 -0700

    Preventing recipe update endpoint from nulling ingredients and steps lists

--- src/main/java/com/foodiesfinds/recipe_service/dto/recipe/RecipeUpdateDTO.java
+++ src/main/java/com/foodiesfinds/recipe_service/dto/recipe/RecipeUpdateDTO.java
@@ -4,9 +4,12 @@ import com.foodiesfinds.recipe_service.dto.base.BaseRecipeDTO;
 import com.foodiesfinds.recipe_service.dto.ingredient.RecipeIngredientUpdateDTO;
 import com.foodiesfinds.recipe_service.dto.instruction.InstructionStepUpdateDTO;
 import com.foodiesfinds.recipe_service.dto.tag.RecipeTagUpdateDTO;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -15,4 +18,16 @@ public class RecipeUpdateDTO extends BaseRecipeDTO<
         RecipeIngredientUpdateDTO,
         RecipeTagUpdateDTO,
         InstructionStepUpdateDTO> {
+
+    @Override
+    @NotEmpty(message = "There must be at least one ingredient.")
+    public List<RecipeIngredientUpdateDTO> getIngredients() {
+        return super.getIngredients();
+    }
+
+    @Override
+    @NotEmpty(message = "There must be at least one step.")
+    public List<InstructionStepUpdateDTO> getSteps() {
+        return super.getSteps();
+    }
 }
